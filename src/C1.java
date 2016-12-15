ba22cf0f87e1e222e78ba802c6911aa7b2b0149f
Adrian <adense13@gmail.com>
2016-12-15 15:42:16 +0100

    C1 wooooorks

--- src/C1.java
+++ src/C1.java
@@ -62,12 +62,14 @@ public static void main(String args[]){
 //	encrypted message (64)= Qe7+h9OPQ7PN9CmF0ZOmD32fwpJotrUL67zxdRvhBn2U3fDtoz4iUGRXNOxwUXdJ2Cmz7zjS0DE8ST5dozBysByz/u1H//iAN+QeGlFVaS1Ee5a/TZilrTCbGPWxfNY4vRXHP6CB82QxhMjQ7/x90/+JLrhdAO99lvmdNetGZjY=
 //	encrypted message (16) = 41eefe87d38f43b3cdf42985d193a60f7d9fc29268b6b50bebbcf1751be1067d94ddf0eda33e2250645734ec70517749d829b3ef38d2d0313c493e5da33072b01cb3feed47fff88037e41e1a5155692d447b96bf4d98a5ad309b18f5b17cd638bd15c73fa081f3643184c8d0effc7dd3ff892eb85d00ef7d96f99d35eb466636
 
-	String pHex = "024100D17B8DC2B327652E3E62FA2696D67328ABC9B8CC226D9E0B14795356D894D613F80E3DA0E152120E70A2CC200B21FD8033E4D99C29AB7AAE77D1C3ED19F46DC9";
-	String qHex = "024100C783076B4577366726CDD5E64059F7C1680CD93E9B935CA38959A3AE5F89A74DC2E3B1D0E4596D905B0F0A6DDEC1D6B678F64A5BBA01667874338D10D56F0997";
+	String pHex = "00f2b0a12a3b39cd599d8fd1a4e77a6caf2e02b6ec51737f4dac7b8ba9ab7d22ed0c039d5282fe1cfbc760a3be435409d3e4fa0b0611c924055a7bead4302e247f";
+	String qHex = "00d17b8dc2b327652e3e62fa2696d67328abc9b8cc226d9e0b14795356d894d613f80e3da0e152120e70a2cc200b21fd8033e4d99c29ab7aae77d1c3ed19f46dc9";
 	BigInteger p = new BigInteger(pHex, 16);
 	BigInteger q = new BigInteger(qHex, 16);
 	BigInteger N = p.multiply(q);
 	System.out.println("N: "+N.toString(16));
+	System.out.println("N length: "+N.toString(16).length());
+	System.out.println("N length hex pairs: "+N.toString(16).length()/2);
 	
 	System.out.println("Now to insert the correct N in our RSA private key...");
 	String censoredHex = "3082025D02010002818100C695C7A7B28ADE75C7A7B28ADE75C7A7B28ADE75C7A7B28ADE7754D4C619E39026E8C3165CECF2292351363E8E9A22008177DFBA8B435421F626F2F47A92A8AA2F3F24F28E0B628064ABFF49C7BA6057626A86BE2EC2F95A4105338BD738A8F297A7E406A14BBD7F5CE9F0E448329789985EEE036D030BDDEE6DAD3BA9D0BAB70203010001028181008A2F0EE6271A28904BA2B6ECBE0463C7BFE6C6EA0307654CE5655AC7F3F9A8B70CB36214F49B6D3D56C7D1B8E1BD3A5FA3B6E7C483D6CB99FE0FC80A79A7C717B9B9ADDAE7E8A20C8C0662604370D7EAE6032AFDDFB5FD4245FC16F80359EE1F689483A8114CF1296E0DBE458C2ABB259E9C56EA7C73205EA6C754698E5E3EE1024100F2B0A12A3B39CD599D8FD1A4E77A6CAF2E02B6EC51737F4DAC7B8BA9AB7D22ED0C039D5282FE1CFBC760A3BE435409D3E4FA0B0611C924055A7BEAD4302E247F024100D17B8DC2B327652E3E62FA2696D67328ABC9B8CC226D9E0B14795356D894D613F80E3DA0E152120E70A2CC200B21FD8033E4D99C29AB7AAE77D1C3ED19F46DC9024100C783076B4577366726CDD5E64059F7C1680CD93E9B935CA38959A3AE5F89A74DC2E3B1D0E4596D905B0F0A6DDEC1D6B678F64A5BBA01667874338D10D56F09970240664F79C505CACFD7EE10592C4FAF1D3F57E785A1ED8EAC09AD4E342DCE49A2E4CDD9E6AF945C8905146DEE54181BD9BBAB9CB1CB5B6EACD45920D21812FD16410240059C4FBB42F4425B8FAFDCEB0890DEB77FE1888C77C2BAFF0BD7946904B95F5D74323A69AEF187A44EEE6F6C51ED48088BCAE5B5C82792B0D98A6245FA130F7D";
@@ -76,9 +78,23 @@ public static void main(String args[]){
 	//the following INTEGER is offset by 139*2=278 hex digits
 	//
 	char[] privateKeyHexChars = censoredHex.toCharArray();
+	String NString = N.toString(16);
+	if(NString.length()/2 < 129){
+		NString = "00"+NString;
+	}
+	System.out.println("New N: "+NString);
+	int newNLength = NString.length()/2;
+	System.out.println("New N length hex: "+newNLength);
 	char[] NChars = N.toString(16).toCharArray();
-	for(int i = 14-1; i<278; i++){
-		privateKeyHexChars[i] = NChars[i-(14-1)];
+	System.out.println("NChars length: "+NChars.length);
+	int forlength = 278-1-14-(3*2);
+	System.out.println("For loop length: "+forlength);
+	int NOffset = 7*2;
+	int NHeader = 3*2;
+	for(int i = 0; i<NChars.length; i++){
+		int temp = i+NOffset+NHeader;
+		System.out.println("Placed in: (char)"+temp+" (hex)"+(temp/2));
+		privateKeyHexChars[i+2+NOffset+NHeader] = NChars[i];
 	}
 	String privateKeyHexRestored = new String(privateKeyHexChars);
 	
@@ -86,6 +102,18 @@ public static void main(String args[]){
 	System.out.println("Key n: "+privateKeyHexRestored);
 	
 	//DECRYPTION
+	//C:\OpenSSL-Win32\bin\openssl.exe
+	
+	//enc -in C:\OpenSSL-Win32\bin\C1msg.txt -out C:\OpenSSL-Win32\bin\C1msgBinary.txt -d -a
+	//rsautl -decrypt -in C:\OpenSSL-Win32\bin\C1msg.b64 -out C:\OpenSSL-Win32\bin\C1Out.txt -inkey C:\OpenSSL-Win32\bin\C1key.pem
+	//rsa 
+	//base64 -d C:\OpenSSL-Win32\bin\blob.b64 > blob
+	//certutil -encode inputFileName encodedOutputFileName
+	
+	/*
+	certutil -decode C:\OpenSSL-Win32\bin\C1msg.txt C:\OpenSSL-Win32\bin\C1msg.b64
+	rsautl -decrypt -in C:\OpenSSL-Win32\bin\C1msg.b64 -out C:\OpenSSL-Win32\bin\C1Out.txt -inkey C:\OpenSSL-Win32\bin\C1key.pem
+	*/
 	//cms -decrypt -in C:\OpenSSL-Win32\bin\C1msg.txt  -recip C:\OpenSSL-Win32\bin\C1key.pem -inkey C:\OpenSSL-Win32\bin\C1key.pem -out C:\OpenSSL-Win32\bin\C1DecryptOut.txt
 	
 	String cryptMsg16 = "41eefe87d38f43b3cdf42985d193a60f7d9fc29268b6b50bebbcf1751be1067d94ddf0eda33e2250645734ec70517749d829b3ef38d2d0313c493e5da33072b01cb3feed47fff88037e41e1a5155692d447b96bf4d98a5ad309b18f5b17cd638bd15c73fa081f3643184c8d0effc7dd3ff892eb85d00ef7d96f99d35eb466636";
